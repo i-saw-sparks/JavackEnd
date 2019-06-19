ea18e210ca86397b4efd2844e93de47b95e7818f
i-saw-sparks <andresmolinaceti@gmail.com>
2019-06-19 09:41:04 -0500

    Basic Order Get

--- src/java/controllers/order.java
+++ src/java/controllers/order.java
@@ -42,56 +42,64 @@ public class order {
     public Response getOrder(
             @QueryParam("id") Integer id, 
             @QueryParam("user") Integer user
-            ) throws SQLException{
+            ){
         Connection conn = Database.getConnection();
         if(id != null){
-            String query = "SELECT * FROM cocollector.\"Orden\" WHERE \"ID\" = ?";
-            PreparedStatement st = conn.prepareStatement(query);
-            st.setInt(1, id);
-            ResultSet rs = st.executeQuery();
-            JSONObject resp = new JSONObject();
-            if(rs.next())
-            {   
-                String userid = rs.getString("ID");
-                String total = rs.getString("Total");
-                String status = rs.getString("Status");
-                String fecha = rs.getString("Fecha_pedido");
-                String direccion = rs.getString("Direccion");
-                String usuario = rs.getString("Usuario");
-                resp.put("ID", userid);
-                resp.put("Total", total);
-                resp.put("Status", status);
-                resp.put("Fecha_pedido", fecha);
-                resp.put("Direccion", direccion);
-                resp.put("Usuario", usuario);
-                
-            }
-            return Response.ok(resp.toJSONString()).build();
-        }else{
-            String query = "SELECT * FROM cocollector.\"Orden\" WHERE \"Usuario\" = ?";
-            PreparedStatement st = conn.prepareStatement(query);
-            st.setInt(1, user);
-            ResultSet rs = st.executeQuery();
-            JSONArray respArr = new JSONArray();
-            while(rs.next())
-            {
+            try {
+                String query = "SELECT * FROM cocollector.\"Orden\" WHERE \"ID\" = ?";
+                PreparedStatement st = conn.prepareStatement(query);
+                st.setInt(1, id);
+                ResultSet rs = st.executeQuery();
                 JSONObject resp = new JSONObject();
-                String total = rs.getString("Total");
-                String status = rs.getString("Status");
-                String fecha = rs.getString("Fecha_pedido");
-                String direccion = rs.getString("Direccion");
-                String usuario = rs.getString("Usuario");
-                resp.put("Total", total);
-                resp.put("Status", status);
-                resp.put("Fecha_pedido", fecha);
-                resp.put("Direccion", direccion);
-                resp.put("Usuario", usuario);
-                respArr.add(resp);
+                if(rs.next())
+                {
+                    String userid = rs.getString("ID");
+                    String total = rs.getString("Total");
+                    String status = rs.getString("Status");
+                    String fecha = rs.getString("Fecha_pedido");
+                    String direccion = rs.getString("Direccion");
+                    String usuario = rs.getString("Usuario");
+                    resp.put("ID", userid);
+                    resp.put("Total", total);
+                    resp.put("Status", status);
+                    resp.put("Fecha_pedido", fecha);
+                    resp.put("Direccion", direccion);
+                    resp.put("Usuario", usuario);
+                    
+                }
+                return Response.ok(resp.toJSONString()).build();
+            } catch (SQLException ex) {
+                Logger.getLogger(order.class.getName()).log(Level.SEVERE, null, ex);
             }
-            return Response.ok(respArr.toJSONString()).build();
+        }else{
+            try {
+                String query = "SELECT * FROM cocollector.\"Orden\" WHERE \"Usuario\" = ?";
+                PreparedStatement st = conn.prepareStatement(query);
+                st.setInt(1, user);
+                ResultSet rs = st.executeQuery();
+                JSONArray respArr = new JSONArray();
+                while(rs.next())
+                {
+                    JSONObject resp = new JSONObject();
+                    String total = rs.getString("Total");
+                    String status = rs.getString("Status");
+                    String fecha = rs.getString("Fecha_pedido");
+                    String direccion = rs.getString("Direccion");
+                    String usuario = rs.getString("Usuario");
+                    resp.put("Total", total);
+                    resp.put("Status", status);
+                    resp.put("Fecha_pedido", fecha);
+                    resp.put("Direccion", direccion);
+                    resp.put("Usuario", usuario);
+                    respArr.add(resp);
+                }
+                return Response.ok(respArr.toJSONString()).build();
+            } catch (SQLException ex) {
+                Logger.getLogger(order.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         
-        //return Response.status(400).build();
+        return Response.status(400).build();
           
     }
     
