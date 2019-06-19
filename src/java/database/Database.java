12d2f516e5f75a8343d76b949f770cbb27a7d2b6
Iván L. Murillo <diskman199@gmail.com>
2019-06-19 08:43:32 -0500

    Ajajjaja olvidé compilar

--- src/java/database/Database.java
+++ src/java/database/Database.java
@@ -5,10 +5,84 @@
  */
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- *
+ * Clase gestora de la base de datos
  * @author ivan_
  */
-public class Database {
+public class Database 
+{ 
+    private static ConcurrentLinkedDeque<Connection> pool;
+    private static Logger logger;
+    private static String database = "BDCocoProyecto";
+    private static String user = "cocollector";
+    private static String password = "12345678";
+    private static String host = "192.168.84.214";
+    private static String port = "5432";
+    private static int initialConnections = 3;
+    private static String url;
     
+    static
+    {
+        logger = Logger.getLogger("Database");
+        pool = new ConcurrentLinkedDeque<>();
+        url = "jdbc:postgresql://"+host+":"+port+"/"+database;
+        System.out.println(url);
+        for(int i=0; i<initialConnections;i++)
+        {
+            try
+            {
+                Connection connection = DriverManager.getConnection(url, user, password);
+                logger.info("Succesfully connected");
+                pool.add(connection);
+            }
+            catch (SQLException ex) 
+            {
+                logger.info("Failed: "+ex.getMessage());
+                ex.printStackTrace();
+            } 
+        }
+    }
+    
+    /**
+     * Pide una conexión a db, si no hay disponibles crea una
+     * @return conexión
+     */
+    public static Connection getConnection()
+    {
+        if(pool.isEmpty())
+        {
+            try 
+            {
+                Connection newConnection = DriverManager.getConnection(url,user,password);
+                logger.info("DB requested");
+                return newConnection;
+            } 
+            catch (SQLException ex) 
+            {
+                logger.info("Failed"+ex.getMessage());
+                return null;
+            }
+        }
+        else 
+        {
+            return pool.getFirst();
+        }
+    }
+    
+    /**
+     * Devuelve una conexión y la reasigna en el stack
+     * @param connection conexion a regresar
+     */
+    public static void returnConnection(Connection connection)
+    {
+        logger.info("Connection returned");
+        pool.addLast(connection);
+    }
 }
