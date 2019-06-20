70c4bc84b155adcb3145e3f2d1732eb90712aafc
i-saw-sparks <andresmolinaceti@gmail.com>
2019-06-20 18:24:08 -0500

    getId

--- src/java/controllers/Tokken.java
+++ src/java/controllers/Tokken.java
@@ -5,6 +5,7 @@
  */
 package controllers;
 
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jose4j.jwa.AlgorithmConstraints;
@@ -21,7 +22,7 @@ import org.jose4j.lang.JoseException;
  */
 public class Tokken {
     
-    
+    private static HashMap<String,String> tabla=new HashMap<String,String>();
     static
     {
         
@@ -49,6 +50,8 @@ public class Tokken {
             jwe.setCompactSerialization(serializedJwe);
             System.out.println("Payload: " + jwe.getPayload());
             
+            tabla.put(serializedJwe,id);
+            
             return serializedJwe;
         } catch (JoseException ex) {
             Logger.getLogger(Tokken.class.getName()).log(Level.SEVERE, null, ex);
@@ -56,30 +59,9 @@ public class Tokken {
         return null;
     }
     
-    public static String getText(String id)
+    public static String getId(String token)
     {
-        try {
-            AesKey key = new AesKey(ByteUtil.randomBytes(16));
-            JsonWebEncryption jwe = new JsonWebEncryption();
-            jwe.setPayload(id);
-            jwe.setAlgorithmHeaderValue(KeyManagementAlgorithmIdentifiers.A128KW);
-            jwe.setEncryptionMethodHeaderParameter(ContentEncryptionAlgorithmIdentifiers.AES_128_CBC_HMAC_SHA_256);
-            jwe.setKey(key);
-            String serializedJwe = jwe.getCompactSerialization();
-            System.out.println("Serialized Encrypted JWE: " + serializedJwe);
-            jwe = new JsonWebEncryption();
-            jwe.setAlgorithmConstraints(new AlgorithmConstraints(AlgorithmConstraints.ConstraintType.WHITELIST,
-                    KeyManagementAlgorithmIdentifiers.A128KW));
-            jwe.setContentEncryptionAlgorithmConstraints(new AlgorithmConstraints(AlgorithmConstraints.ConstraintType.WHITELIST,
-                    ContentEncryptionAlgorithmIdentifiers.AES_128_CBC_HMAC_SHA_256));
-            jwe.setKey(key);
-            jwe.setCompactSerialization(serializedJwe);
-            System.out.println("Payload: " + jwe.getPayload());
-           
-            return serializedJwe;
-        } catch (JoseException ex) {
-            Logger.getLogger(Tokken.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
+        System.out.println(tabla.get(token));
+        return tabla.get(token);
     }
 }
