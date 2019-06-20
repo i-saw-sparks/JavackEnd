4bd061ffe491d48b9b0d30b414399bbe015387f8
XV02 <lghhssi@gmail.com>
2019-06-20 13:08:14 -0500

    Requisition final

--- src/java/controllers/requisition.java
+++ src/java/controllers/requisition.java
@@ -78,7 +78,16 @@ public class requisition {
         return Response.status(400).build(); 
     }
     
-    /// El post sin terminar
+    /**
+     * Por medio del InputStream recibe todos los valores
+     * necesarios para crear un nuevo pedido.
+     * Realiza la query insertando los datos recibidos y en caso
+     * de realizarse de manera correcta envía un código 200 de confirmación
+     * y retorna el ID del pedido recién creado.
+     * Si ocurre algún error al momento de la inserción regresa un 400.
+     * @param input
+     * @return 
+     */
     @POST
     public Response createReq(InputStream input){
         try {
@@ -97,8 +106,8 @@ public class requisition {
                 st = conn.prepareStatement(query);
                 st.setInt(1, Integer.parseInt(jsonObject.get("total").toString()));
                 st.setInt(2, Integer.parseInt(jsonObject.get("cantidad").toString()));
-                st.setInt(4, (Integer.parseInt(jsonObject.get("orden").toString())));
-                st.setInt(5, (Integer.parseInt(jsonObject.get("producto").toString())));
+                st.setInt(3, Integer.parseInt(jsonObject.get("orden").toString()));
+                st.setInt(4, Integer.parseInt(jsonObject.get("producto").toString()));
                 ResultSet rs = st.executeQuery();
                 JSONObject resp = new JSONObject();
                 if(rs.next()){
@@ -118,26 +127,76 @@ public class requisition {
         return Response.status(400).build();
     }
     
-    /// El put sin terminar
+    /**
+     * Por medio del InputStream recibe todos los valores
+     * necesarios para modificar un pedido.
+     * Primero verifica que la id estè entre los valores.
+     * Realiza un SELECT a la base de datos para obtener los datos previos
+     * y los guarda, para posteriormente modificar los valores solo si vienen 
+     * en los paràmetros y si no están vacíos.
+     * Retorna un código 200 si se hizo la modificación correctamente.
+     * Si ocurre algún error al momento de la inserción regresa un 400.
+     * @param input
+     * @return 
+     */
     @PUT
     public Response modifyReq(InputStream input){
         try {
+            Connection conn = Database.getConnection();
             JSONParser jsonParser = new JSONParser();
             JSONObject jsonObject = (JSONObject)jsonParser.parse(new InputStreamReader(input, "UTF-8"));
-            
-            //Aqui se toman los argumentos y se realiza la query
-            
-            return Response.ok().build();
-            
+            if(jsonObject.containsKey("id"))
+            {
+                String query = "SELECT * FROM cocollector.\"Pedido\" WHERE \"ID\" = ?";
+                PreparedStatement st = conn.prepareStatement(query);
+                st.setInt(1, Integer.parseInt(jsonObject.get("id").toString()));
+                ResultSet rs = st.executeQuery();
+                String total = "", cantidad = "", orden = "", producto = "";
+                if(rs.next())
+                {
+                    total = rs.getString("Total");
+                    cantidad = rs.getString("Cantidad");
+                    orden = rs.getString("Orden");
+                    producto = rs.getString("Producto");                   
+                }
+                total = (jsonObject.containsKey("total")) ? (!(jsonObject.get("total").toString().equals("")) ? jsonObject.get("total").toString():total):total;
+                cantidad = (jsonObject.containsKey("cantidad")) ? (!(jsonObject.get("cantidad").toString().equals("")) ? jsonObject.get("cantidad").toString():cantidad):cantidad;
+                orden = (jsonObject.containsKey("orden")) ? (!(jsonObject.get("orden").toString().equals("")) ? jsonObject.get("orden").toString():orden):orden;
+                producto = (jsonObject.containsKey("producto")) ? (!(jsonObject.get("producto").toString().equals("")) ? jsonObject.get("producto").toString():producto):producto;
+                query = "UPDATE cocollector.\"Pedido\" SET \"Total\" = ?, "
+                    + "\"Cantidad\" = ?, "
+                    + "\"Orden\" = ?, "
+                    + "\"Producto\" = ? WHERE \"ID\" = ?";
+                st = conn.prepareStatement(query);
+                st.setInt(1, Integer.parseInt(total));
+                st.setInt(2, Integer.parseInt(cantidad));
+                st.setInt(3, Integer.parseInt(orden));
+                st.setInt(4, Integer.parseInt(producto));
+                st.setInt(5, Integer.parseInt(jsonObject.get("id").toString()));
+                st.execute();
+                return Response.ok().build();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(order.class.getName()).log(Level.SEVERE, null, ex);
         } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(order.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException | ParseException ex) {
-            Logger.getLogger(order.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(requisition.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParseException ex) {
+            Logger.getLogger(requisition.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(requisition.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return Response.serverError().build();
-    }
+        return Response.status(400).build(); 
+      }
     
-    /// El delete esta terminado pero sin probar
+    /**
+     * Recibe como parámetro un ID y posteriormente
+     * realiza un delete en la base de datos donde el ID
+     * sea igual al recibido. 
+     * Si se realiza correctamente regresa un código 200 de confirmación
+     * y si ocurre algún percance un 400 de error.
+     * @param id
+     * @return 
+     */
     @DELETE
     public Response deleteOrder(@QueryParam("id") Integer id){
         Connection conn = Database.getConnection();
